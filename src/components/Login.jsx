1da4f867bd5c39c598461bb8ea713a919757fdb5
Rafiikhwan28 <ikhwandani088@gmail.com>
2025-05-26 14:44:50 +0700

    Fix case sensitivity issue with Login.css

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import './login.css'; // Pastikan nama file sesuai persis
+import "./Login.css";
 
 function Login() {
   const navigate = useNavigate();
